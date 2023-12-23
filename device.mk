d95a8f7abe7208abdc136dc12633128b2c600c26
pimpmaneaton <shaunsibu@gmail.com>
2023-12-23 02:25:16 -0700

    Some fixes
    Change-Id: I4c970cefb5d6efecfd0790be8afd461bf55b8b7c

--- device.mk
+++ device.mk
@@ -668,6 +668,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.use_color_management=tr
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.protected_contents=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.display_update_imminent_timeout_ms=50
 
+# USB
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += sys.usb.config=adb
+
 # force to blend in P3 mode
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.sys.sf.native_mode=2 \
